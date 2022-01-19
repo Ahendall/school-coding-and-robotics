dc751394c661402fe53a0dec98091b5c9d8caf4e
Ahendall <adham1.eldalil@gmail.com>
2022-01-19 17:25:23 +0400

    ppap

--- Grade 8/C_C++/Bootleg-McDs/main.cpp
+++ Grade 8/C_C++/Bootleg-McDs/main.cpp
@@ -4,6 +4,8 @@
 // Function prototypes
 int newCustomer(Customer customer);
 int existingCustomer(Customer customer);
+int orderLoop(Customer customer);
+
 
 // Main function to handle Instancing and customer newcheck
 int main() {
@@ -18,6 +20,7 @@ int main() {
         existingCustomer(customer);
 }
 
+
 // Defining function prototypes
 int newCustomer(Customer customer) {
     // Print welcome message and menu
@@ -25,50 +28,11 @@ int newCustomer(Customer customer) {
     cout << "Here is our menu for today:\n\n";
     printMenu();
 
-    // Infinite loop to get customer's orders
-    while (true) {
-        string query = lower(getString(notext));
-
-        // Customer wants to remove an order from cart
-        if (slice(query, 0, 2) == "rm") {
-            int status = customer.remove(strToInt(slice(query, 3)));
-
-            if (status == 0)
-                cout << "Item " << query[3] << " removed successfully. Anything else?\n";
-
-            else if (status == 1)
-                cout << "Item " << query[3] << " is not a valid order Id. Please try again.\n";
-
-            else
-                cout << "Item " << query[3] << " is not in your cart. Please try again.\n";
-        }
-
-        // Customer wants to checkout and pay
-        else if (query.find("checkout") != string::npos) {
-            bool status = customer.checkout();
-            if (status) {
-                cout << customer.checkoutMsg() << endl;
-                return 0;
-            } else {
-                cout << "You do not have enough money!\nTry removing a few things.";
-                continue;
-            }
-        }
-
-        // Customer wants to add a new order to cart
-        else {
-            int orderId = strToInt(query);
-            bool orderStatus = customer.order(orderId);
-
-            if (not orderStatus)
-                cout << "I am not familiar with the order \"" << orderId << "\". Please try again." << endl;
-
-            else
-                excellentChoice();
-        }
-    }
+	return orderLoop(customer);
 }
 
+
+// Same as above, but adds option for customer to re-order their previous order
 int existingCustomer(Customer customer) {
     cout << "Welcome back, " << customer.name << ". Your remaining balance is: $" << customer.cash << "\n";
     cout << "Here is our menu for today:\n\n";
@@ -95,7 +59,13 @@ int existingCustomer(Customer customer) {
         cout << "Try ordering something else. \n";
     }
 
-    while (true) {
+	return orderLoop(customer);
+}
+
+
+// Loop that above functions will use to allow user to order
+int orderLoop(Customer customer) {
+	while (true) {
         string query = lower(getString(notext));
 
         // Customer wants to remove an order from cart
