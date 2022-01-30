769875c12302665251f0134c2cc09ef89ecf4db3
Ahendall <adham1.eldalil@gmail.com>
2022-01-30 22:13:46 +0400

    Started new django project for summative 0

---
+++ Grade 8/Unit0/homepage/views.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import authenticate, login, logout
+from django import forms
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+# Create your views here.
+def index(request):
+	pass
+
+def login(request):
+	pass
+
+def signup(request):
+	pass
