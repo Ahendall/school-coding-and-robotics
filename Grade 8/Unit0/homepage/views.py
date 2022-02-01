d284ad804e7215e0c30a8cf32bb4d565883d3d5c
Ahendall <adham1.eldalil@gmail.com>
2022-02-01 22:43:30 +0400

    Navbar tests

--- Grade 8/Unit0/homepage/views.py
+++ Grade 8/Unit0/homepage/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request,"homepage.html")
 
 def login(request):
     pass
