2b7b448a9181dbfce546dbf468196b4d26ee6f8f
Ahendall <adham1.eldalil@gmail.com>
2022-02-10 14:51:50 +0400

    Fixed shit and broke more shit. idk

--- Grade 8/Unit0/movies/urls.py
+++ Grade 8/Unit0/movies/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'movies'
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('<str:movie_name>/', views.detail, name='detail'),
-]
+    path('', views.index, name='index'),
+    path('<str:movie_name>', views.detail, name='detail'),
+    # path('<str:movie_name>/<str:action>', views.detail, name='action'),
+] 
--- Grade 8/Unit0/movies/views.py
+++ Grade 8/Unit0/movies/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from homepage import models
 
 # Create your views here.
+
+
 def index(request):
     # convert movies into a matrix
     movies = models.Movie.objects.all()
@@ -19,16 +21,55 @@ def index(request):
         'movieMatrix': movie_matrix,
     })
 
-def detail(request, movie_name):
-	try:
-    	movie = models.Movie.objects.get(linkName=movie_name)
-	except:
-		return render(request, 'movies/detail.html', {
+# This is where all the data
+def detail(request, movie_name, action=None):
+    # Exception handle in case no movie is returned
+    try:
+        movie = models.Movie.objects.get(linkName=movie_name)
+    except:
+        return render(request, 'movies/detail.html', {
             'movie': None,
         })
 
-    # check if movie is empty
-    
+    # Check if user has liked or disliked the movie
+    if action == 'like' and movie.title not in request.session['likedMovies'] and request.method == 'POST':
+        request.session['likedMovies'].append(movie.title)
+
+        # remove movie from disliked list if it is there
+        if movie.title in request.session['dislikedMovies']:
+            request.session['dislikedMovies'].remove(movie.title)
+
+        # Update genreWeightedScore
+        for genre in movie.genre.split(','):
+            if genre in request.session['genreWeightedScore']:
+                request.session['genreWeightedScore'][genre] += 1
+            else:
+                request.session['genreWeightedScore'][genre] = 1
+
+        return render(request, 'movies/detail.html', {
+            'movie': movie,
+            'like': True
+        })
+
+    elif action == 'dislike' and movie.title not in request.session['dislikedMovies'] and request.method == 'POST':
+        request.session['dislikedMovies'].append(movie.title)
+
+        # Remove movie from liked movies if it is there
+        if movie.title in request.session['likedMovies']:
+            request.session['likedMovies'].remove(movie.title)
+
+        # Update genreWeightedScore
+        for genre in movie.genre.split(','):
+            if genre in request.session['genreWeightedScore']:
+                request.session['genreWeightedScore'][genre] -= 1
+            else:
+                request.session['genreWeightedScore'][genre] = -1
+
+        return render(request, 'movies/detail.html', {
+            'movie': movie,
+            'dislike': True,
+        })
+
     return render(request, 'movies/detail.html', {
         'movie': movie,
     })
