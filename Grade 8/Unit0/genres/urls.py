5b1cab1acafae4b9b1226276fbeba233b4e0b401
Ahendall <adham1.eldalil@gmail.com>
2022-02-11 09:12:45 +0400

    Created genres page and removed a bunch of pycache

--- Grade 8/Unit0/genres/urls.py
+++ Grade 8/Unit0/genres/urls.py
@@ -1,6 +1,11 @@
-# Create urls for the following genres: action, comedy, drama, horror, fantasy
-from django.conf.urls import url
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 app_name = 'genres'
+urlpatterns = [
+    path('action/', views.action, name='action'),
+    path('comedy/', views.comedy, name='comedy'),
+    path('drama/', views.drama, name='drama'),
+    path('horror/', views.horror, name='horror'),
+    path('fantasy/', views.fantasy, name='fantasy'),
+]
--- Grade 8/Unit0/genres/views.py
+++ Grade 8/Unit0/genres/views.py
@@ -1,3 +1,46 @@
+from django.contrib.auth import authenticate, login, logout
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db.models import Q
+
+from homepage import models
 
 # Create your views here.
+def action(request):
+    # Get list of all movies with "Action" genre
+    movies = models.Movie.objects.filter(genre="Action")
+    return render(request, 'genres/index.html', {
+        'action': movies,
+    })
+
+def comedy(request):
+    # Get list of all movies with "Comedy" genre
+    movies = models.Movie.objects.filter(genre="Comedy")
+    return render(request, 'genres/index.html', {
+        'comedy': movies,
+    })
+
+def drama(request):
+    # Get list of all movies with "Drama" genre
+    movies = models.Movie.objects.filter(genre="Drama")
+    return render(request, 'genres/index.html', {
+        'drama': movies,
+    })
+
+def horror(request):
+    # Get list of all movies with "Horror" genre
+    movies = models.Movie.objects.filter(genre="Horror")
+    return render(request, 'genres/index.html', {
+        'horror': movies,
+    })
+
+def fantasy(request):
+    # Get list of all movies with "Fantasy" genre
+    movies = models.Movie.objects.filter(genre="Fantasy")
+    return render(request, 'genres/index.html', {
+        'fantasy': movies,
+    })
