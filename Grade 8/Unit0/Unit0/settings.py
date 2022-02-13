e552babd4cf69e141e37615826eb6cc872348118
Ahendall <adham1.eldalil@gmail.com>
2022-02-13 22:59:07 +0400

    added footers to layout files

--- Grade 8/Unit0/Unit0/settings.py
+++ Grade 8/Unit0/Unit0/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pgn$8k0$bjf%igl&6l4u^65%e4()4q%&3r9g&0!s1blea6@ih-
 DEBUG = True
 SESSION_SAVE_EVERY_REQUEST = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
