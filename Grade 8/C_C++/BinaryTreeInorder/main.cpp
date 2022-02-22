cd1eb19f7eb031daf03de86d3895d1e370dc996e
Ahendall <adham1.eldalil@gmail.com>
2022-02-22 20:33:24 +0400

    did an interative function using  a stack

--- Grade 8/C_C++/BinaryTreeInorder/main.cpp
+++ Grade 8/C_C++/BinaryTreeInorder/main.cpp
@@ -26,6 +26,21 @@ void printTreeNodesRecusivly(treeNode *node) {
 		printTreeNodesRecusivly(node->rightChild);
 }
 
+// Print nodes iteratively
 void printTreeNodesIterative(treeNode *node) {
+	stack<treeNode *> s;
+	treeNode *temp = node;
+	while (temp != NULL || !s.empty()) {
+		while (temp != NULL) {
+			s.push(temp);
+			temp = temp->leftChild;
+		}
+		temp = s.top();
+		s.pop();
+		cout << temp->value << endl;
+		temp = temp->rightChild;
+	}
 
+	// Time complexity: O(n)
+	// Space complexity: O(n)
 }
