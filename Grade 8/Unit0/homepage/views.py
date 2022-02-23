8c425257f0964efb22871b3848021646397e97a2
Ahendall <adham1.eldalil@gmail.com>
2022-02-23 09:10:52 +0400

    Excluded grammar and common words from recs algo

--- Grade 8/Unit0/homepage/views.py
+++ Grade 8/Unit0/homepage/views.py
@@ -6,7 +6,6 @@ from django.urls import reverse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.db.models import Q
-
 from . import models
 
 # Create your views here.
@@ -70,21 +69,41 @@ def index(request):
 
 	# get movies from movies where titles are similar to titles in likedMovies
 	recommendedMovies = []
+
+	# Make sure likedMovies has no duplicates
+	likedMovies = list(set(request.session['likedMovies']))
+	print(f'DEBUG INFO (SESSION) >> {request.session["likedMovies"]}')
+
 	for movie in movies:
 		for likedMovie in request.session['likedMovies']:
 			# split likedMovie into array of words
 			likedMovie = likedMovie.split()
 
-			# exclude the word "the" from likedMovie
-			if "the" in likedMovie:
-				likedMovie.remove("the")
+			# exclude common words and articles from likedMovie title
+			for word in likedMovie:
+				if word.lower() in ['the', 'a', 'an', 'and', 'or', 'but', 'for', 'of', 'nor', 'on', 'to', 'with']:
+					likedMovie.remove(word)
+
+			# remove the word "the" from the movie title
+			# need this bcos previous thing isn't working on the word "the"
+			if 'the' in likedMovie:
+				likedMovie.remove('the')
+			elif 'The' in likedMovie:
+				likedMovie.remove('The')
+
+			# Remove punctuation from likedMovie title
+			for word in likedMovie:
+				if not word.isalpha():
+					likedMovie.remove(word)
+
+
+			print(f'DEBUG INFO (LIKED MOVIE) >> {likedMovie}')
 
 			# get movies with similar titles
 			for word in likedMovie:
 				if word in movie.title:
 					recommendedMovies.append(movie)
 					break
-
 	# Remove any duplicates from recommendedMovies
 	recommendedMovies = list(dict.fromkeys(recommendedMovies))
 
@@ -112,6 +131,12 @@ def index(request):
 				recommendedMovies, key=lambda x: request.session['genreWeightedScore'][x.genre], reverse=True)
 
 	# Shortening recommended movies and re-sorting by rating
+	if len(recommendedMovies) < 6:
+		movies = sorted(
+				movies, key=lambda x: x.rating, reverse=True)
+		
+		recommendedMovies = concatenate(recommendedMovies, movies)
+
 	recommendedMovies = recommendedMovies[:6]
 	recommendedMovies = sorted(
 			recommendedMovies, key=lambda x: x.rating, reverse=True)
@@ -154,3 +179,9 @@ def allsame(array):
 			break
 
 	return True if same else False
+
+def concatenate(arr1, arr2):
+	for element in arr2:
+		arr1.append(element)
+
+	return arr1
