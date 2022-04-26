165739563aa37d06bc4f53daca3df824d5e313ee
Ahendall <adham1.eldalil@gmail.com>
2022-04-26 14:15:44 +0400

    Seperating code into different functions

--- Grade 8/Python/Summative Preview/main.py
+++ Grade 8/Python/Summative Preview/main.py
@@ -1,100 +1,24 @@
-from cs50 import SQL
-from random import randint
-from sys import exit
-
-import menu as m
-from helpers import customer, get_int
-
-# Sqlite init
-db = SQL("sqlite:///customers.db")
-
+import util
+import Customer
+import os
+import sys
 
 def main():
-    # Instancing class `customer`
-    Customer = customer(input("What is your name?\n").lower())
+    customer = Customer.Customer(
+        input("What is your name?\n").lower()
+    )
 
-    if Customer.isNew == True:
-        # Customer is new, order_prev is disabled and different prompt
-        print(
-            f"Welcome, {Customer.name}. Since you are a new Customer, here is $1000.00 to spend here.\nHere is our menu for today:"
-        )
-        print(m.menu)
-        print(m.orderPrompt)
-
-        # Getting order
-        while True:
-            query = input().lower()
-
-            if query[0:2] == "rm":
-                Customer.rm(int(query[3]))
-                continue
-            elif "checkout" in query:
-                Customer.checkout()
-                return
-
-            # Checking if order is valid. If yes, running order()
-            try:
-                if int(query) in m.menuDict.keys():
-                    Customer.order(int(query))
-                    continue
-                else:
-                    print(
-                        f'I am not familiar with the order "{query}"\n Please order again.'
-                    )
-                    continue
-            except:
-                print("Please input a valid order.")
-
+    if customer.isNew:
+        return newCustomer(customer)
     else:
-        # Customer already exists in db
-        # order_prev now enabled
-        print(
-            f"Welcome back, {Customer.name}. Your current balance is ${Customer.cash}\nHere is our menu for today:"
-        )
-        print(m.menu)
+        return returningCustomer(customer)
 
-        # Checking if customer wants re-order or new order
-        re_order = get_int(
-            "To re-order your previous order, type 1.\nIf you would like to make a new order, type 2."
-        )
-        if re_order == 1:
-            checkoutStatus = Customer.order(Customer.prev_order)
-            if checkoutStatus:
-                exit(0)
-            print(
-                "You do not have enough money to order your previous order.\nTry ordering something new."
-            )
+def returningCustomer(customer: Customer.Customer) -> int:
+    print(f"Welcome back, \033[32n{customer.name}!\033[0m")
+    orderChoice = util.getInt("To make a new order, type in 1. To order previous order, type in 2.")
 
-        print(m.orderPrompt)
+def newCustomer(customer: Customer.Customer) -> int:
+    pass
 
-        # Getting order
-        while True:
-            query = input().lower()
-
-            # If User wants to remove or checkout
-            if query[0:2] == "rm":
-                Customer.rm(int(query[3]))
-                continue
-            elif "checkout" in query:
-                checkoutStatus = Customer.checkout()
-                if checkoutStatus == False:
-                    print("You do not have enough money! Try removing a few items.")
-                    continue
-                return
-
-            # Checking if order is valid. If yes, running order()
-            try:
-                if int(query) in m.menuDict.keys():
-                    Customer.order(int(query))
-                    continue
-                else:
-                    print(
-                        f'I am not familiar with the order "{query}"\n Please order again.'
-                    )
-                    continue
-            except:
-                print("Please input a valid order.")
-
-
-if __name__ == "__main__":
-    main()
+def orderLoop(customer: Customer.Customer) -> int:
+    pass
