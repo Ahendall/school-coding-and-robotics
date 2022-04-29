34f575868dd593cc240a997081eacf6811163c66
Ahendall <adham1.eldalil@gmail.com>
2022-04-29 08:17:17 +0400

    Removed C_C++

--- Grade 8/Python/Summative Preview/customer_schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE sqlite_sequence(name,seq); -- Idk what this is used for but i found it when i ran .schema lol
-
-CREATE TABLE customers (
-   id INTEGER PRIMARY KEY AUTOINCREMENT,
-   name TEXT NOT NULL,
-   cash DECIMAL DEFAULT 1000.00,
-   prev_order TEXT,
-   prev_price DECIMAL
-);
